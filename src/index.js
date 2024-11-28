be98dfa6ecfaafe073f91b019e36d8d2a0437d4c
Rue Ire <reerine20@msn.com>
2024-11-28 08:42:41 +0200

    edit: server to index
    renamed server to index
    added app.get
    untested

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//npm install express sqlite3
+const express = require('express')
+const app = express()
+const port = 3000
+const db = require("./db")
+
+const fetchQuery = `SELECT * FROM words`;
+app.use(express.json()); // Parses incoming JSON requests
+
+app.get("/", (req, res) => {
+    db.all(fetchQuery, (err, result) => {
+        if (err) {
+            console.error("Error fetching data")
+            res.status(500).json({ error: "Failed to fetch words" });
+            return;
+        }
+        res.json(result);
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+process.on("SIGINT", () => {
+    db.end();
+    console.log("Database connection closed."); //debug
+    process.exit(0);
+});
