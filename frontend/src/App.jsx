2aa144be0f6e134ca1ecaa600d0e6e0fda5e6222
Rue Ire <reerine20@msn.com>
2024-12-05 09:24:10 +0200

    edit: App.jsx
    form to div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,19 @@ import FetchWords from './components/FetchWords';
 import './App.css'; 
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import ButtonLink from './components/Buttonlink';
+import { useEffect } from 'react';
 
 function App() {
 
   function HomeView() {
     return<>
-    <form className='main-form'>
+      <div className='main-form'>
       <h1>Language Learning Application</h1>
       <div className='home-buttons'>
           <ButtonLink to="/user">User Page</ButtonLink>
           {/* <button id='admin'>Admin Material</button> */}
       </div>
-    </form>
+    </div>
     </>
   };
 
