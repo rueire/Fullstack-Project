8823cedae113787ece8822c64261090083ee0862
Rue Ire <reerine20@msn.com>
2024-12-05 10:30:09 +0200

    Edit: UserView.jsx
    Add new ButtonLink

--- frontend/src/components/UserView.jsx
+++ frontend/src/components/UserView.jsx
@@ -5,7 +5,7 @@ export default function UserView() {
     <h1 id="user-title"> User View</h1>
     <div className="user-choices">
         <ButtonLink to="/engfinn">English to Finnish Assignment</ButtonLink>
-        {/* <button id="finneng_btn"> Finnish to English</button> */}
+        <ButtonLink to="/finneng"> Finnish to English</ButtonLink>
     </div>
     </>
 }
