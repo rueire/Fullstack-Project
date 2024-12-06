97e8dee5c35b8ed4ba7730341a6743fab4778929
Rue Ire <reerine20@msn.com>
2024-12-06 10:07:59 +0200

    Edit: ButtonLink
    now has id parameter

--- frontend/src/components/Buttonlink.jsx
+++ frontend/src/components/Buttonlink.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
-export default function ButtonLink({ to, children }) {
-    return <Link to={to}><button>{children}</button></Link>;
+export default function ButtonLink({ to, id, children }) {
+    return <Link to={to}>
+        <button id={id}>{children}</button>
+        </Link>;
 }
