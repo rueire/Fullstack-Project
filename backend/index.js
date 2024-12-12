c693160df8fbe99482b5be22ea72d977f6e7f667
Rue Ire <reerine20@msn.com>
2024-12-12 13:14:47 +0200

    update index.js
    added put, delete and post

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,7 @@ const port = 3000
 const cors = require('cors'); // Import the cors package
 const db = require("./database")
 const path = require("path");
-
-const fetchQuery = `SELECT * FROM words`;
+const { copyFileSync } = require('fs');
 
 app.use(cors({
     origin: 'http://localhost:5173' // Allow only your React app's origin
@@ -19,6 +18,7 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 
 app.get("/api", (req, res) => {
+    const fetchQuery = `SELECT * FROM words`;
     db.all(fetchQuery, (err, result) => {
         if (err) {
             console.error("Error fetching data", err)
@@ -29,9 +29,58 @@ app.get("/api", (req, res) => {
     })
 });
 
+//add words
+app.post("/api/words", (req, res) => {
+    const { eng_word, finn_word } = req.body;
+    const insertWords = `INSERT INTO words (eng_word, finn_word) VALUES (?, ?)`;
+
+    db.run(insertWords, [eng_word, finn_word], (err, result) => {
+        if (err) {
+            console.error("Error inserting data", err)
+            res.status(500).json({ error: "Failed to insert words", details: err.message });
+            return;
+        }
+        res.json(result);
+        console.log("new pair added")
+    })
+});
+
+//edit words
+app.put("/api/words/:id", (req, res) => {
+    const { id } = req.params;
+    const { eng_word, finn_word } = req.body;
+    const editWords = `UPDATE words SET eng_word = ?, finn_word = ? WHERE id = ?`;
+
+    db.run(editWordsWords, [eng_word, finn_word, id], (err) => {
+        if (err) {
+            console.error("Error updating data", err)
+            res.status(500).json({ error: "Failed to edit words", details: err.message });
+            return;
+        }
+        res.status(201).json({ id: this.lastID, eng_word, finn_word });
+        console.log("pair edited")
+    })
+});
+
+//delete words
+app.delete("/api/words/:id", (req, res) => {
+    const { id } = req.params;
+    const deleteWords = `DELETE FROM words WHERE id = ?`;
+
+    db.run(deleteWords, [id], (err) => {
+        if (err) {
+            console.error("Error deleting data", err)
+            res.status(500).json({ error: "Failed to delete words", details: err.message });
+            return
+        }
+        res.status(204).send();
+        console.log("word pair deleted");
+    })
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
 
 process.on("SIGINT", () => {
     db.close();
